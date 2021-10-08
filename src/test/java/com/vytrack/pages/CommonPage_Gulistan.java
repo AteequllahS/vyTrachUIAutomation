d67cb5513ac2b941f70917fce65bc5255e96b28d
gulistan33 <gulistan33@gmail.com>
2021-10-08 00:34:38 -0700

    small improvement

---
+++ src/test/java/com/vytrack/pages/CommonPage_Gulistan.java
@@ -0,0 +1,46 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.BrowserUtil;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CommonPage_Gulistan {
+
+
+    public CommonPage_Gulistan(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//div[@class='message'])[2]")
+    public WebElement alertMassage;
+
+
+
+
+
+    public void navigateToModule(String tab, String module) {
+        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
+        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
+        try {
+            BrowserUtil.waitForClickablility(By.xpath(tabLocator), 5);
+            WebElement tabElement = Driver.getDriver().findElement(By.xpath(tabLocator));
+            new Actions(Driver.getDriver()).moveToElement(tabElement).pause(200).perform();
+        } catch (Exception e) {
+            BrowserUtil.clickWithWait(By.xpath(tabLocator), 5);
+        }
+        try {
+            BrowserUtil.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
+            BrowserUtil.waitForVisibility(By.xpath(moduleLocator), 5);
+            BrowserUtil.scrollToElement(Driver.getDriver().findElement(By.xpath(moduleLocator)));
+            Driver.getDriver().findElement(By.xpath(moduleLocator)).click();
+        } catch (Exception e) {
+//            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
+            BrowserUtil.clickWithTimeOut(Driver.getDriver().findElement(By.xpath(moduleLocator)),  5);
+        }
+    }
+
+}
---
+++ src/test/java/com/vytrack/tests/US_09_Gulistan/CreateVehicleContract_Gulistan.java
@@ -0,0 +1,81 @@
+package com.vytrack.tests.US_09_Gulistan;
+
+import com.vytrack.pages.CommonPage_Gulistan;
+import com.vytrack.pages.VLoginPage;
+import com.vytrack.pages.AllVehicleContractPage_Gulistan;
+import com.vytrack.pages.VehicleContractPage_Guistan;
+import com.vytrack.utilities.BrowserUtil;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class CreateVehicleContract_Gulistan extends TestBase {
+
+    @Test
+    public void managerCreateContract() {
+
+        VLoginPage vLoginPage = new VLoginPage();
+        String username = ConfigurationReader.read("storeManager1");
+        String password = ConfigurationReader.read("password");
+        vLoginPage.login(username, password);
+
+        CommonPage_Gulistan commonPage = new CommonPage_Gulistan();
+        commonPage.navigateToModule("Fleet", "Vehicle Contracts");
+
+
+        AllVehicleContractPage_Gulistan allVehicleContractPage = new AllVehicleContractPage_Gulistan();
+        BrowserUtil.waitFor(5);
+        allVehicleContractPage.contractBtn.click();
+
+
+        VehicleContractPage_Guistan vehicleContractPage = new VehicleContractPage_Guistan();
+        //vehicleContractPage.chooseCalenderDate();
+        vehicleContractPage.fillForm();
+        vehicleContractPage.SaveAndCloseBtn.click();
+
+        //Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String actualMassage = vehicleContractPage.flashMassage.getText();
+        Assertions.assertEquals("Entity saved",vehicleContractPage.flashMassage.getText());
+    }
+
+
+
+    @Test
+    public void driverNotCreateContract(){
+
+        VLoginPage vLoginPage = new VLoginPage();
+        String username = ConfigurationReader.read("truckDriver1");
+        String password = ConfigurationReader.read("password");
+        vLoginPage.login(username, password);
+
+        CommonPage_Gulistan commonPage = new CommonPage_Gulistan();
+        commonPage.navigateToModule("Fleet", "Vehicle Contracts");
+
+        //Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        Assertions.assertEquals("You do not have permission to perform this action.", commonPage.alertMassage.getText());
+
+
+
+    }
+}
+
+
+
+
+
+
+
+/*
+9. Story: As a user, I should be create Vehicle Contract. AC #1: managers are able to Create Vehicle Contract.
+        Given store/sales manager is on the homePage
+        When user select “Vehicle Contract” under Fleet module
+        And user click “Create Vehicle Contract” button
+        When user fill out general information and click “Save and Close” button
+        Then verify “Entity saved” confirm message
+
+ */
